7bd853e61b4825361fae0cd1f2ec4853e2ac90a9
Max Radke <maxradke@rocketmail.com>
2021-03-07 23:27:40 -0800

    Added code for test5, only ints

--- leapyear.py
+++ leapyear.py
@@ -3,12 +3,13 @@
 # Class: CS 362     Assignment: Homework 7
 # Description: Takes an input and tells if it is a leap year
 
-import math
 
 global Number # Input number variable (str)
 global x # Input number variable (int)
 
 def ly(x):
+    if not isinstance(x, int):
+        raise TypeError
     Number = str(x)
     
     if x % 4 == 0: # Calculate if leap year
